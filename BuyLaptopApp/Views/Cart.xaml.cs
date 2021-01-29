a439c47b394877fc159672c2f831d199001bf735
tinhtrinh <chumeoconxauxa@gmail.com>
2021-01-29 22:03:36 +0700

    kết nối api cho giỏ hàng

--- BuyLaptopApp/Views/Cart.xaml.cs
+++ BuyLaptopApp/Views/Cart.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,9 @@ namespace BuyLaptopApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Cart : ContentPage
     {
+        string sohd, sodt, trigia;
+        List<Laptop> carts = new List<Laptop>();
+
         public Cart()
         {
             InitializeComponent();
@@ -22,9 +26,15 @@ namespace BuyLaptopApp.Views
         public void HienThi()
         {
             Database db = new Database();
-            List<Laptop> carts = db.LayThongTinLaptop();
+            List<KhachHang> kh = new List<KhachHang>();
+
+            carts = db.LayThongTinLaptop();
+            kh = db.LayThongTinKH();
+
             lstcart.ItemsSource = carts;
+            sodt = kh[0].SODT;
             sum.Text = tinhTongGia(carts).ToString() + "vnd";
+            trigia = tinhTongGia(carts).ToString();
         }
 
         public long tinhTongGia(List<Laptop> carts)
@@ -37,7 +47,6 @@ namespace BuyLaptopApp.Views
                     tong += int.Parse(cart.GIA);
                 }
             }
-            
             return tong;
         }
 
@@ -57,9 +66,39 @@ namespace BuyLaptopApp.Views
             }
         }
 
-        private void Btnbook_Clicked(object sender, EventArgs e)
+        public async void themct(List<Laptop> carts, string sohd)
         {
-            Navigation.PushAsync(new PayNow());
+            HttpClient http = new HttpClient();
+            foreach (Laptop cart in carts)
+            {
+                await http.GetStringAsync("http://www.qllt.somee.com/api/serviceController/themCTHD?sohd="+ sohd +"&malt=" + cart.MALT);
+            }
+        }
+
+        private async void Btnbook_Clicked(object sender, EventArgs e)
+        {
+            if(trigia != "0")
+            {
+                Random r = new Random();
+                sohd = r.Next(2, 100).ToString();
+
+                HttpClient http = new HttpClient();
+                var kq = await http.GetStringAsync("http://www.qllt.somee.com/api/serviceController/themHD?sohd=" + sohd + "&sodt=" + sodt + "&trigia=" + trigia);
+                if (kq != "[{\"Column1\":\"fail\"}]")
+                {
+                    await DisplayAlert("Thông báo", "Đặt hàng thành công", "Đồng ý");
+                    themct(carts, sohd);
+                    await Navigation.PushAsync(new PayNow());
+                }
+                else
+                {
+                    await DisplayAlert("Thông báo", "Đặt hàng thất bại. Vui lòng thử lại", "Đồng ý");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Thông báo", "Đặt hàng thất bại. Vui lòng thử lại", "Đồng ý");
+            }
         }
     }
 }
