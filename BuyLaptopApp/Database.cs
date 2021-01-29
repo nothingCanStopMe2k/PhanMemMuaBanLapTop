927c61227af2c69694cc1450fd83e22ab6993b50
tinhtrinh <chumeoconxauxa@gmail.com>
2021-01-29 11:07:49 +0700

    kết nối api đăng nhập, api , nút đăng xuất cho trang cá nhân

--- BuyLaptopApp/Database.cs
+++ BuyLaptopApp/Database.cs
@@ -85,5 +85,34 @@ namespace BuyLaptopApp
                 return false;
             }
         }
+
+        public bool Them_Khachhang(KhachHang kh)
+        {
+            try
+            {
+                string duongdan = System.IO.Path.Combine(folder, "laptop.db");
+                var connection = new SQLiteConnection(duongdan);
+                connection.Insert(kh);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<KhachHang> LayThongTinKH()
+        {
+            try
+            {
+                string duongdan = System.IO.Path.Combine(folder, "laptop.db");
+                var connection = new SQLiteConnection(duongdan);
+                return connection.Table<KhachHang>().ToList();
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
